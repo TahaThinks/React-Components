603678977b30b9dd3f2e389766a9eac32edfab1a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-04-10 22:20:15 +0400

    Added Component File

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import the Custom Components from the JSX files created
-import App from "./App";
+import App from "./components/App";
 // React allows us to divide sections of the page to Components
 // each containig its own HTML,CSS & JS. This way we can use the
 // Component in multiple projects.
