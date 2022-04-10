3b266acb52e3544f1ddf909646ba43e608b51896
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2022-04-10 21:24:30 +0400

    Added List Component

---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+
+//Export the function to be used in other files
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//import the Custom Components from the JSX files created
 import Heading from "./Heading";
+import List from "./List";
 // React allows us to divide sections of the page to Components
 // each containig its own HTML,CSS & JS. This way we can use the
 // Component in multiple projects.
@@ -8,11 +10,7 @@ import Heading from "./Heading";
 ReactDOM.render(
   <div>
     <Heading />
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <List />
   </div>,
   document.getElementById("root")
 );
